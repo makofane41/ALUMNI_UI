754720ea082d281d798f22b30eb2ff4832bcb841
olatunbosun <107124814+OLAYINKA102@users.noreply.github.com>
2022-09-22 16:19:16 +0200

    alumni project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/alumni'),
+      dir: require('path').join(__dirname, './coverage/profile'),
       subdir: '.',
       reporters: [
         { type: 'html' },
